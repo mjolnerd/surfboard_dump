09ae9e7927f7f6858dc645a3a776c072a8541000
Matt Shultz <github.com@watchingyouwatching.me>
2017-10-14 13:17:22 -0700

    Refactored the string list pull into a function.

--- surfboard_dump.py
+++ surfboard_dump.py
@@ -6,37 +6,49 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 from elasticsearch import Elasticsearch
 
-
 epoch = datetime.utcfromtimestamp(0)
 this_run_ts = datetime.now()
 this_run_ts_millis = (this_run_ts - epoch).total_seconds() * 1000.0
 
-# These are the pages from the modem and fields we want to record
-#  Signals is a complicated enough page that I may break that out to a different process.
-datapulls = {}
-datapulls['http://192.168.100.1/cmHelpData.htm'] = ['Model Name','Vendor Name', 'Firmware Name', 'Boot Version', 'Hardware Version', 'Serial Number', 'Firmware Build Time']
-
-# This is a dictionary to compile the stuff we will end up sending up to elasticsearch
-fields = {}
-fields['timestamp'] = this_run_ts_millis
-
-# We are going to store our results in Elasticsearch.
-es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-# Set up a session for our pull from the modem
-s = requests.session()
-
-# Loop through the pages we want to snag data from
-for url in datapulls.keys():
-    qfields = datapulls[url]
-    response = s.get(url)
+# Looks for a list of strings in the body of a page
+#  Returns a dict of the found strings and their value after the ':'
+def ExtractStringList(session, url, sl):
+    fields = {}
+    response = session.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    # Loop through td tags
-    for td in soup.find("td"):
-        for qfield in qfields:
-            if qfield in td:
-                s = str(td)
-                s = s.split(':',1)
-                s = map(str.strip, s)
-                fields[s[0]] = s[1]
+    for line in soup.body.get_text().split('\n'):
+        for needle in sl:
+            if needle in line:
+                line = line.split(':',1)
+                line = map(unicode.strip, line)
+                fields[line[0]] = line[1]
+    return fields
 
-print fields
+# Takes a session and url
+#  Returns a dict comprised key:values from tables on the page
+def ExtractFromTables(session, url):
+    fields = {}
+    
+    return fields
+
+
+def main():
+    # This is a dictionary to compile the stuff we will end up sending up to elasticsearch
+    fields = {}
+    fields['timestamp'] = this_run_ts_millis
+    # Set up a session for our pull from the modem
+    s = requests.session()
+
+
+    # Grab from the Help page
+    fields.update(ExtractStringList(s,'http://192.168.100.1/cmHelpData.htm', ['Model Name','Vendor Name', 'Firmware Name', 'Boot Version', 'Hardware Version', 'Serial Nu    mber', 'Firmware Build Time']))
+
+    print fields
+    s.close()
+    # We are going to store our results in Elasticsearch.
+    #es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+
+
+if __name__ == '__main__':
+    main()
+
