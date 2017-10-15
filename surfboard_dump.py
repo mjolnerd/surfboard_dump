900332368dc57d3b0ecec2a0e7d2e5921d105a2c
Matt Shultz <github.com@watchingyouwatching.me>
2017-10-15 07:47:41 -0700

    Upstream and Signal Stats tables now processed.
    Lots of code reuse here, could probably be refactored into something
    more elegant...

--- surfboard_dump.py
+++ surfboard_dump.py
@@ -51,10 +51,8 @@ def ExtractSignals(session, url):
     soup = BeautifulSoup(response.text, 'html.parser')
 
     for table in soup.findAll('table'):
-        print 'NEW TABLE ---------'
         head = 'Downstream'
         if head in table.get_text() and len(table.findAll('tr')) > 1:
-            print table
             fields[head] = {}
             # Load up a list of lists with our data
             t_temp = []
@@ -70,10 +68,13 @@ def ExtractSignals(session, url):
                     rcount += 1
             # Transpose rows -> columns
             tt_temp = zip(*t_temp)
-            labels = ['null', 'Frequency (Hz)', 'Signal to Noise Ratio (dB)', 'Downstream Modulation', 'Power Level (DBmV)']
+            labels = ['null',
+                    'Frequency (Hz)',
+                    'Signal to Noise Ratio (dB)',
+                    'Downstream Modulation',
+                    'Power Level (DBmV)']
             # Won't need these anymore...
             tt_temp.pop(0)
-            pprint.pprint(tt_temp)
             # Step through each channel data and Store in fields
             for cdat in tt_temp:
                 channel = int(cdat[0])
@@ -94,9 +95,101 @@ def ExtractSignals(session, url):
                 fc = 4
                 val = int(cdat[fc].split(' ')[0])
                 fields[head][channel][labels[fc]] = val
+
         head = 'Upstream'
+        if head in table.get_text() and len(table.findAll('tr')) > 1:
+            fields[head] = {}
+            # Load up a list of lists with our data
+            t_temp = []
+            rcount = 0
+            for tr in table.findAll('tr'):
+                if tr.findAll('td'):
+                    t_temp.append([])
+                    for td in tr.findAll('td',recursive=False):
+                        t_temp[rcount].append(td.get_text().strip())
+                    # Doing this shenanigans because of a recursive table issue...
+                    if len(t_temp[rcount]) < 2:
+                        t_temp.pop(rcount)
+                    rcount += 1
+            # Transpose rows -> columns
+            tt_temp = zip(*t_temp)
+            labels = ['null',
+                    'Frequency (Hz)',
+                    'Ranging Service ID',
+                    'Symbol Rate (Msym/sec)',
+                    'Power Level (DBmV)',
+                    'Upstream Modulation',
+                    'Ranging Status']
+            # Won't need these anymore...
+            tt_temp.pop(0)
+            # Step through each channel data and Store in fields
+            for cdat in tt_temp:
+                channel = int(cdat[0])
+                fields[head][channel] = {}
+                # Freq
+                fc = 1
+                val = int(cdat[fc].split(' ')[0])
+                fields[head][channel][labels[fc]] = val
+                # Range Service ID
+                fc = 2
+                val = int(cdat[fc].split(' ')[0])
+                fields[head][channel][labels[fc]] = val
+                # Symbol Rate
+                fc = 3
+                val = float(cdat[fc].split(' ')[0])
+                fields[head][channel][labels[fc]] = val
+                # Power Level
+                fc = 4
+                val = int(cdat[fc].split(' ')[0])
+                fields[head][channel][labels[fc]] = val
+                # Upstream Modulation
+                fc = 5
+                val = cdat[fc].split('\n')
+                fields[head][channel][labels[fc]] = val
+                # Ranging Status
+                fc = 6
+                val = cdat[fc]
+                fields[head][channel][labels[fc]] = val
+
         head = 'Signal Stats'
-    pprint.pprint(fields)
+        if head in table.get_text() and len(table.findAll('tr')) > 1:
+            fields[head] = {}
+            # Load up a list of lists with our data
+            t_temp = []
+            rcount = 0
+            for tr in table.findAll('tr'):
+                if tr.findAll('td'):
+                    t_temp.append([])
+                    for td in tr.findAll('td',recursive=False):
+                        t_temp[rcount].append(td.get_text().strip())
+                    # Doing this shenanigans because of a recursive table issue...
+                    if len(t_temp[rcount]) < 2:
+                        t_temp.pop(rcount)
+                    rcount += 1
+            # Transpose rows -> columns
+            tt_temp = zip(*t_temp)
+            labels = ['null',
+                    'Total Unerrored Codewords',
+                    'Total Correctable Codewords',
+                    'Total Uncorrectable Codewords']
+            # Won't need these anymore...
+            tt_temp.pop(0)
+            # Step through each channel data and Store in fields
+            for cdat in tt_temp:
+                channel = int(cdat[0])
+                fields[head][channel] = {}
+                # Unerrored
+                fc = 1
+                val = int(cdat[fc])
+                fields[head][channel][labels[fc]] = val
+                # Correctable
+                fc = 2
+                val = int(cdat[fc])
+                fields[head][channel][labels[fc]] = val
+                # Uncorrectable
+                fc = 3
+                val = int(cdat[fc])
+                fields[head][channel][labels[fc]] = val
     return fields
 
 
@@ -109,16 +202,25 @@ def main():
 
 
     # Grab from the Help page
-    fields.update(ExtractStringList(s,'http://192.168.100.1/cmHelpData.htm', ['Model Name','Vendor Name', 'Firmware Name', 'Boot Version', 'Hardware Version', 'Serial Number', 'Firmware Build Time']))
-    table_pulls = ['http://192.168.100.1/indexData.htm', 'http://192.168.100.1/cmAddressData.htm', 'http://192.168.100.1/cmConfigData.htm','http://192.168.100.1/cmOpenSourceData.htm']
+    helplabels=['Model Name',
+            'Vendor Name',
+            'Firmware Name',
+            'Boot Version',
+            'Hardware Version',
+            'Serial Number',
+            'Firmware Build Time']
+    fields.update(ExtractStringList(s,'http://192.168.100.1/cmHelpData.htm', helplabels))
+    table_pulls = ['http://192.168.100.1/indexData.htm',
+            'http://192.168.100.1/cmAddressData.htm',
+            'http://192.168.100.1/cmConfigData.htm']
     for url in table_pulls:
         fields.update(ExtractFromTables(s, url))
     fields.update(ExtractSignals(s, 'http://192.168.100.1/cmSignalData.htm'))
     s.close()
-    #pprint.pprint(fields)
+    pprint.pprint(fields)
     # We are going to store our results in Elasticsearch.
     #es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-
+# END def MAIN
 
 if __name__ == '__main__':
     main()
