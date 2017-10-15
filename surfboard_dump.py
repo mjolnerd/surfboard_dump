cf357ea1414b7a2204f2e30fa355c65ddad9e935
Matt Shultz <github.com@watchingyouwatching.me>
2017-10-14 22:15:02 -0700

    Got the first Signal section sorted.  Others should be easier...I hope.

--- surfboard_dump.py
+++ surfboard_dump.py
@@ -26,7 +26,7 @@ def ExtractStringList(session, url, sl):
     return fields
 
 # Takes a session and url
-#  Returns a dict comprised key:values from tables on the page
+#  Returns a dict of key:values from tables on the page
 def ExtractFromTables(session, url):
     fields = {}
     response = session.get(url)
@@ -39,6 +39,64 @@ def ExtractFromTables(session, url):
                 key = td[0].get_text().strip()
                 value = td[1].get_text().strip()
                 fields[key] = value
+    return fields
+
+# Takes a session and url
+#  Returns a dict with sensible structure for the Signals page
+#  This page has a unique layout, so this won't be as flexible as the other
+#  functions.  Will end up returning a multi-level dictionary.
+def ExtractSignals(session, url):
+    fields = {}
+    response = session.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    for table in soup.findAll('table'):
+        print 'NEW TABLE ---------'
+        head = 'Downstream'
+        if head in table.get_text() and len(table.findAll('tr')) > 1:
+            print table
+            fields[head] = {}
+            # Load up a list of lists with our data
+            t_temp = []
+            rcount = 0
+            for tr in table.findAll('tr'):
+                if tr.findAll('td'):
+                    t_temp.append([])
+                    for td in tr.findAll('td',recursive=False):
+                        t_temp[rcount].append(td.get_text().strip())
+                    # Doing this shenanigans because of a recursive table issue...
+                    if len(t_temp[rcount]) < 2:
+                        t_temp.pop(rcount)
+                    rcount += 1
+            # Transpose rows -> columns
+            tt_temp = zip(*t_temp)
+            labels = ['null', 'Frequency (Hz)', 'Signal to Noise Ratio (dB)', 'Downstream Modulation', 'Power Level (DBmV)']
+            # Won't need these anymore...
+            tt_temp.pop(0)
+            pprint.pprint(tt_temp)
+            # Step through each channel data and Store in fields
+            for cdat in tt_temp:
+                channel = int(cdat[0])
+                fields[head][channel] = {}
+                # Freq
+                fc = 1
+                val = int(cdat[fc].split(' ')[0])
+                fields[head][channel][labels[fc]] = val
+                # SNR
+                fc = 2
+                val = int(cdat[fc].split(' ')[0])
+                fields[head][channel][labels[fc]] = val
+                # Downstream Modulation
+                fc = 3
+                val = cdat[fc]
+                fields[head][channel][labels[fc]] = val
+                # Power Level
+                fc = 4
+                val = int(cdat[fc].split(' ')[0])
+                fields[head][channel][labels[fc]] = val
+        head = 'Upstream'
+        head = 'Signal Stats'
+    pprint.pprint(fields)
     return fields
 
 
@@ -55,8 +113,9 @@ def main():
     table_pulls = ['http://192.168.100.1/indexData.htm', 'http://192.168.100.1/cmAddressData.htm', 'http://192.168.100.1/cmConfigData.htm','http://192.168.100.1/cmOpenSourceData.htm']
     for url in table_pulls:
         fields.update(ExtractFromTables(s, url))
+    fields.update(ExtractSignals(s, 'http://192.168.100.1/cmSignalData.htm'))
     s.close()
-    pprint.pprint(fields)
+    #pprint.pprint(fields)
     # We are going to store our results in Elasticsearch.
     #es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
