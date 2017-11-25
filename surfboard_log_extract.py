7c1865f5216e5438527de3ec73ae1d9a7ac349d2
Matt Shultz <github.com@watchingyouwatching.me>
2017-11-25 13:31:29 -0700

    Got a basic layout setup

--- surfboard_log_extract.py
+++ surfboard_log_extract.py
@@ -28,163 +28,38 @@ from elasticsearch import Elasticsearch
 #  Returns a dict with sensible structure for the Logs page
 #  We don't have access to a flatfile text log, because Cox. So there will
 #  be some shenanigans to avoid duplicates.
-def ExtractSignals(session, url):
-    fields = {}
+def ExtractLogs(session, url):
+    fields = []
     response = session.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    for table in soup.findAll('table'):
-        head = 'Downstream'
-        if head in table.get_text() and len(table.findAll('tr')) > 1:
-            fields[head] = {}
-            # Load up a list of lists with our data
-            t_temp = []
-            rcount = 0
-            for tr in table.findAll('tr'):
-                if tr.findAll('td'):
-                    t_temp.append([])
-                    for td in tr.findAll('td',recursive=False):
-                        t_temp[rcount].append(td.get_text().strip())
-                    # Doing this shenanigans because of a recursive table issue...
-                    if len(t_temp[rcount]) < 2:
-                        t_temp.pop(rcount)
-                    rcount += 1
-            # Transpose rows -> columns
-            tt_temp = zip(*t_temp)
-            labels = ['null',
-                    'Frequency (Hz)',
-                    'Signal to Noise Ratio (dB)',
-                    'Downstream Modulation',
-                    'Power Level (DBmV)']
-            # Won't need these anymore...
-            tt_temp.pop(0)
-            # Step through each channel data and Store in fields
-            for cdat in tt_temp:
-                channel = int(cdat[0])
-                fields[head][channel] = {}
-                # Freq
-                fc = 1
-                val = int(cdat[fc].split(' ')[0])
-                fields[head][channel][labels[fc]] = val
-                # SNR
-                fc = 2
-                val = int(cdat[fc].split(' ')[0])
-                fields[head][channel][labels[fc]] = val
-                # Downstream Modulation
-                fc = 3
-                val = cdat[fc]
-                fields[head][channel][labels[fc]] = val
-                # Power Level
-                fc = 4
-                val = int(cdat[fc].split(' ')[0])
-                fields[head][channel][labels[fc]] = val
-
-        head = 'Upstream'
-        if head in table.get_text() and len(table.findAll('tr')) > 1:
-            fields[head] = {}
-            # Load up a list of lists with our data
-            t_temp = []
-            rcount = 0
-            for tr in table.findAll('tr'):
-                if tr.findAll('td'):
-                    t_temp.append([])
-                    for td in tr.findAll('td',recursive=False):
-                        t_temp[rcount].append(td.get_text().strip())
-                    # Doing this shenanigans because of a recursive table issue...
-                    if len(t_temp[rcount]) < 2:
-                        t_temp.pop(rcount)
-                    rcount += 1
-            # Transpose rows -> columns
-            tt_temp = zip(*t_temp)
-            labels = ['null',
-                    'Frequency (Hz)',
-                    'Ranging Service ID',
-                    'Symbol Rate (Msym/sec)',
-                    'Power Level (DBmV)',
-                    'Upstream Modulation',
-                    'Ranging Status']
-            # Won't need these anymore...
-            tt_temp.pop(0)
-            # Step through each channel data and Store in fields
-            for cdat in tt_temp:
-                channel = int(cdat[0])
-                fields[head][channel] = {}
-                # Freq
-                fc = 1
-                val = int(cdat[fc].split(' ')[0])
-                fields[head][channel][labels[fc]] = val
-                # Range Service ID
-                fc = 2
-                val = int(cdat[fc].split(' ')[0])
-                fields[head][channel][labels[fc]] = val
-                # Symbol Rate
-                fc = 3
-                val = float(cdat[fc].split(' ')[0])
-                fields[head][channel][labels[fc]] = val
-                # Power Level
-                fc = 4
-                val = int(cdat[fc].split(' ')[0])
-                fields[head][channel][labels[fc]] = val
-                # Upstream Modulation
-                fc = 5
-                val = cdat[fc].split('\n')
-                fields[head][channel][labels[fc]] = val
-                # Ranging Status
-                fc = 6
-                val = cdat[fc]
-                fields[head][channel][labels[fc]] = val
-
-        head = 'Signal Stats'
-        if head in table.get_text() and len(table.findAll('tr')) > 1:
-            fields[head] = {}
-            # Load up a list of lists with our data
-            t_temp = []
-            rcount = 0
-            for tr in table.findAll('tr'):
-                if tr.findAll('td'):
-                    t_temp.append([])
-                    for td in tr.findAll('td',recursive=False):
-                        t_temp[rcount].append(td.get_text().strip())
-                    # Doing this shenanigans because of a recursive table issue...
-                    if len(t_temp[rcount]) < 2:
-                        t_temp.pop(rcount)
-                    rcount += 1
-            # Transpose rows -> columns
-            tt_temp = zip(*t_temp)
-            labels = ['null',
-                    'Total Unerrored Codewords',
-                    'Total Correctable Codewords',
-                    'Total Uncorrectable Codewords']
-            # Won't need these anymore...
-            tt_temp.pop(0)
-            # Step through each channel data and Store in fields
-            for cdat in tt_temp:
-                channel = int(cdat[0])
-                fields[head][channel] = {}
-                # Unerrored
-                fc = 1
-                val = int(cdat[fc])
-                fields[head][channel][labels[fc]] = val
-                # Correctable
-                fc = 2
-                val = int(cdat[fc])
-                fields[head][channel][labels[fc]] = val
-                # Uncorrectable
-                fc = 3
-                val = int(cdat[fc])
-                fields[head][channel][labels[fc]] = val
+    for table in soup.find_all('table'):
+        # Iterate through the logs table, row by row
+        if 'Priority' in table.get_text() and len(table.find_all('tr')) > 1:
+            for tr in table.find_all('tr'):
+                ftmp = []
+                if 'Priority' not in tr.get_text():
+                    ftmp.append(tr.find_all('td')[0].get_text())
+                    ftmp.append(tr.find_all('td')[1].get_text())
+                    ftmp.append(tr.find_all('td')[2].get_text())
+                    ftmp.append(tr.find_all('td')[3].get_text())
+                if len(ftmp) > 0:
+                    fields.append(ftmp)
+            # fix timestamps
+            # drop duplicate entries
+            # convert to dict
+    print fields
     return fields
 
 
 def main():
     this_run_ts = datetime.utcnow().isoformat()
     # This is a dictionary to compile the stuff we will end up sending up to elasticsearch
-    fields = {}
-    fields['timestamp'] = this_run_ts
+    fields = []
     # Set up a session for our pull from the modem
     s = requests.session()
 
-    fields.update(ExtractSignals(s, 'http://192.168.100.1/cmLogsData.htm'))
+    fields.append(ExtractLogs(s, 'http://192.168.100.1/cmLogsData.htm'))
     s.close()
 
 
