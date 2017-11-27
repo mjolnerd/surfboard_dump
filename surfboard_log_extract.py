a751b3799e2bb2c2b4188a616f6338c6d4645e63
Matt Shultz <github.com@watchingyouwatching.me>
2017-11-27 11:25:54 -0700

    Fixing timestamps after a modem restart
    When the SB6141 reboots, it defaults to Epoch until it can get a time
    signal.  This means our timestamps are all funky.  Going to add the
    offset to the last known good timestamp and call it "close enough".

--- surfboard_log_extract.py
+++ surfboard_log_extract.py
@@ -2,13 +2,14 @@
 
 '''
 Map this using:
-PUT surfboard6141
+PUT surfboard6141_logs
 {
   "mappings": {
-    "stats":{
+    "logs":{
       "properties": {
         "timestamp":{
-          "type": "date"
+          "type": "date",
+          "format": "MMM d yyyy HH:mm:ss"
         }
       }
     }
@@ -28,7 +29,7 @@ from elasticsearch import Elasticsearch
 #  Returns a dict with sensible structure for the Logs page
 #  We don't have access to a flatfile text log, because Cox. So there will
 #  be some shenanigans to avoid duplicates.
-def ExtractLogs(session, url):
+def ExtractLogs(session, url, last_ts):
     fields = []
     response = session.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -37,38 +38,49 @@ def ExtractLogs(session, url):
         # Iterate through the logs table, row by row
         if 'Priority' in table.get_text() and len(table.find_all('tr')) > 1:
             for tr in table.find_all('tr'):
-                ftmp = []
+                ftmp = {}
                 if 'Priority' not in tr.get_text():
-                    ftmp.append(tr.find_all('td')[0].get_text())
-                    ftmp.append(tr.find_all('td')[1].get_text())
-                    ftmp.append(tr.find_all('td')[2].get_text())
-                    ftmp.append(tr.find_all('td')[3].get_text())
+                    ftmp['timestamp'] = tr.find_all('td')[0].get_text()
+                    ftmp['priority'] = tr.find_all('td')[1].get_text()
+                    ftmp['code'] = tr.find_all('td')[2].get_text()
+                    ftmp['message'] = tr.find_all('td')[3].get_text()
                 if len(ftmp) > 0:
                     fields.append(ftmp)
-            # fix timestamps
+
+            # initing a valid value for this on the edgecase that earliest log entry is not valid
+            last_ts = datetime.now()
+            # Moving from oldest to newest
+            for entry in reversed(fields):
+                ts = datetime.strptime(entry['timestamp'],'%b %d %Y %H:%M:%S')
+                if str(ts.year) == '1970':
+                    epoch = datetime.utcfromtimestamp(0)
+                    ts_offset = ts - epoch
+                    ts = last_ts + ts_offset
+                else:
+                    last_ts = ts
+                print ts
             # drop duplicate entries
-            # convert to dict
-    print fields
+
     return fields
 
 
 def main():
-    this_run_ts = datetime.utcnow().isoformat()
-    # This is a dictionary to compile the stuff we will end up sending up to elasticsearch
     fields = []
+    # Get the timestamp for the last entry we placed into elasticsearch
+    es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+
+
     # Set up a session for our pull from the modem
     s = requests.session()
 
-    fields.append(ExtractLogs(s, 'http://192.168.100.1/cmLogsData.htm'))
+    fields = ExtractLogs(s, 'http://192.168.100.1/cmLogsData.htm','catfood')
     s.close()
 
-
-    # We are going to store our results in Elasticsearch.
-    es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
     #pprint.pprint(fields)
-    json_string = json.dumps(fields)
+    json_string = json.dumps(fields[0])
+    print json_string
     #Lets hold off on making our es indexes icky until we test this...
-    #es.index(index = 'surfboard6141', doc_type = 'logs', body = json_string)
+    #es.index(index = 'surfboard6141_logs', doc_type = 'logs', body = json_string)
 # END def MAIN
 
 if __name__ == '__main__':
