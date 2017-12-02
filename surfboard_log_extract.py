7b884280ff05e74625957c392395caf28b729b75
Matt Shultz <github.com@watchingyouwatching.me>
2017-12-02 13:11:05 -0700

    Avoid storing duplicate log entries.
    This is still pretty rough.  Needs some try-catch blocks and could be
    more robust in general.  The timezone handling is pretty vulgar, mostly
    because timezones in general are kind of nasty in Python right now.

--- surfboard_log_extract.py
+++ surfboard_log_extract.py
@@ -9,6 +9,10 @@ PUT surfboard6141_logs
       "properties": {
         "timestamp":{
           "type": "date",
+          "format": "date_time_no_millis"
+        },
+        "timestamp_raw":{
+          "type": "date",
           "format": "MMM d yyyy HH:mm:ss"
         }
       }
@@ -32,6 +36,7 @@ from elasticsearch import Elasticsearch
 #  be some shenanigans to avoid duplicates.
 def ExtractLogs(session, url, last_ts):
     fields = []
+    rfields = []
     response = session.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
@@ -41,52 +46,60 @@ def ExtractLogs(session, url, last_ts):
             for tr in table.find_all('tr'):
                 ftmp = {}
                 if 'Priority' not in tr.get_text():
-                    ftmp['timestamp'] = tr.find_all('td')[0].get_text()
+                    ftmp['timestamp_raw'] = tr.find_all('td')[0].get_text()
                     ftmp['priority'] = tr.find_all('td')[1].get_text()
                     ftmp['code'] = tr.find_all('td')[2].get_text()
                     ftmp['message'] = tr.find_all('td')[3].get_text()
                 if len(ftmp) > 0:
                     fields.append(ftmp)
 
-            last_ts = 'NULL'
+            last_log_ts = 'NULL'
             # Moving from oldest to newest
             for entry in reversed(fields):
-                ts = datetime.strptime(entry['timestamp'],'%b %d %Y %H:%M:%S')
+                ts = datetime.strptime(entry['timestamp_raw'],'%b %d %Y %H:%M:%S')
                 if str(ts.year) == '1970':
                     epoch = datetime.utcfromtimestamp(0)
                     ts_offset = ts - epoch
-                    # Oops, we don't have a last_ts yet.
+                    # Oops, we don't have a last_log_ts yet.
                     # Look for a more recent timestamp and work backwards from there.
-                    if str(last_ts) == 'NULL':
+                    if str(last_log_ts) == 'NULL':
                         for i in reversed(fields):
-                            tc = datetime.strptime(i['timestamp'],'%b %d %Y %H:%M:%S')
+                            tc = datetime.strptime(i['timestamp_raw'],'%b %d %Y %H:%M:%S')
                             if str(tc.year) == '1970':
                                 pass
                             else:
-                                last_ts = tc
+                                last_log_ts = tc
                                 break
-                        # Still nothing?  Ok, manually set it to now.
-                        if str(last_ts) == 'NULL':
-                            last_ts = datetime.now()
+                        # Still nothing?  Ok, manually set it to now-ish.
+                        if str(last_log_ts) == 'NULL':
+                            last_log_ts = datetime.now().replace(microsecond=0)
                         # Offset backwards from future ts
-                        ts = last_ts - ts_offset
+                        ts = last_log_ts - ts_offset
                         # Reset this so these don't stack (yes it is inefficient, CPU is cheap, for now...)
-                        last_ts = 'NULL'
+                        last_log_ts = 'NULL'
                     else:
                         # Offset forwards from past ts
-                        ts = last_ts + ts_offset
+                        ts = last_log_ts + ts_offset
                 else:
-                    last_ts = ts
+                    last_log_ts = ts
                 # Now that is sorted, lets make this timezone aware
                 tz_loc = tz.tzlocal()
                 ts = ts.replace(tzinfo = tz_loc)
                 tsutc = ts.astimezone(tz.tzutc())
-                entry['timestamp_utc'] = tsutc.isoformat()
-                #print 'ts: {} / ts_utc: {}'.format(ts, entry['timestamp_utc'])
-
-            # drop duplicate entries
-
-    return fields
+                # Have we already recorded this into elastic search?
+                if tsutc <= last_ts:
+                    print 'Skipping already recorded entry:'
+                    pprint.pprint(entry)
+                else:
+                    # Update stored timestamp to our fixed up UTC stamp
+                    entry['timestamp'] = tsutc.isoformat()
+                    print 'Saving for elasticsearch storage:'
+                    pprint.pprint(entry)
+                    # store to the list we're actually going to return
+                    rfields.append(entry)
+    print '#### rfields:'
+    pprint.pprint(rfields)
+    return rfields
 
 
 def main():
@@ -94,18 +107,24 @@ def main():
     # Get the timestamp for the last entry we placed into elasticsearch
     es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
-
     # Set up a session for our pull from the modem
     s = requests.session()
 
-    fields = ExtractLogs(s, 'http://192.168.100.1/cmLogsData.htm','catfood')
+    # Get the timestamp for the most recent record. Used to remove duplicate entries on the cable modem.
+    last_record = es.search(index='surfboard6141_logs', body={"query": {"match_all": {}},"size": 1,"_source": "timestamp","sort":[{"timestamp": {"order": "desc"}}]})
+    # We're only puting UTC in and should only be getting UTC out...
+    last_ts = last_record['hits']['hits'][0]['_source']['timestamp'].replace('+00:00','')
+    last_ts = datetime.strptime(last_ts,'%Y-%m-%dT%H:%M:%S').replace(tzinfo = tz.tzutc())
+    print last_ts
+    fields = ExtractLogs(s, 'http://192.168.100.1/cmLogsData.htm', last_ts)
     s.close()
 
-    #pprint.pprint(fields)
-    json_string = json.dumps(fields[0])
-    print json_string
-    #Lets hold off on making our es indexes icky until we test this...
-    #es.index(index = 'surfboard6141_logs', doc_type = 'logs', body = json_string)
+    for field in fields:
+        json_string = json.dumps(field)
+        print 'Storing to elasticsearch:'
+        print json_string
+        #Lets hold off on making our es indexes icky until we test this...
+        es.index(index = 'surfboard6141_logs', doc_type = 'logs', body = json_string)
 # END def MAIN
 
 if __name__ == '__main__':
